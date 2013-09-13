4ba7121e3806fa88ae477c7e0ab1b42cfb462107
zoggop <zoggop@gmail.com>
2013-09-13 16:22:00 -0700

    still nothing functional

--- zcompile.pl
+++ zcompile.pl
@@ -1,20 +1,99 @@
 # zoggop dot com static website compiler
 
-# read old checksums to check against
-my @oldchecksums;
-if (-e "posts.checksums") {
-	open(FILE, "posts.checksums")
+# config variables
+my $frontPageDays = 7; # how many days beyond the first post to display on the front page
+
+# read last checksums and dates to compare against
+my %lastCheckSums {};
+my %lastDates = {};
+if (-e "posts.inventory") {
+	open(FILE, "posts.inventory")
 	@cslines = <FILE>;
-	close(FILE)
+	close(FILE);
+	my $a = 0;
+	my $filename;
+	my $checksum;
+	foreach my $line (@cslines) {
+		chomp($line);
+		if ($a == 0) {
+			$filename = $line;
+			$a = 1; 
+		} elsif ($a == 1) {
+			$lastCheckSums{$filename} = $line;
+			$a = 2;
+		} else {
+			$lastDates{$filename} = $line;
+			$a = 0;
+		}
+	}
 }
+
+# ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+
+my $newestPost = GetLastNewestPost();
 
 # read post markdown checksums to find new posts
 opendir(DIR, "posts")
 @postfiles = <DIR>;
 closedir(DIR)
+my @newPosts;
 foreach my $postfile (@postfiles) {
-	my $checksum = md5_hex(do { local $/; IO::File->new("filename")->getline });
+	my $checksum = md5_hex(do { local $/; IO::File->new($postfile)->getline });
+	if ($checksum ne $lastCheckSums{$postfile}) {
+		push(@newPosts, $postfile);
+	}
+}
 
+# read new posts and create their post pages
+my @pages = ();
+my %pageRef = {};
+my $ref = 0;
+foreach my $postfile (@newPosts) {
+	my $page = {};
+	($page->{'Title'}, $page->{'Date'}, $page->{'Content'}) = ReadPost($postfile);
+	$page->{'md'} = $postfile;
+	$pages[$ref] = $page;
+	$pageRef{$postfile} = $ref;
+	BuildPostPage($postfile);
+	$ref = $ref + 1;
+}
+
+
+#
+# SUBROUTINES
+#
+
+# build a post page
+sub BuildPostPage() {
+	my $ref = $pageRef{$_[0]};
+	my %page = %{ $pages[$ref] };
+	foreach my $key (keys %page) { $buffer{$key} = $page{$key}; }
+	
+}
+
+# read a post
+sub ReadPost() {
+	my $postfile = $_[0];
+	if (-e $postfile) {
+
+		return ($title, $date, $content);
+	} else {
+		return;
+	}
+}
+
+# read last newest post
+sub GetLastNewestPost() {
+	if (-e "posts.newest") {
+		open(FILE, "posts.newest")
+		my @nplines = <FILE>;
+		close(FILE);
+		my $postfile = $nplines[0];
+		chomp($postfile);
+		return $postfile;
+	} else {
+		return;
+	}
 }
 
 # parse a template, takes a filename and uses data from %buffer
